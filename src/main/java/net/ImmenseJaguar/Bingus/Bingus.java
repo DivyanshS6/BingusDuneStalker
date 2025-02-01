488102098dfe719781c66739a1528925cd07a880
DivyanshS6 <Dshukla1370@gmail.com>
2025-01-31 23:36:08 -0500

    added stuffffffffff

--- src/main/java/net/ImmenseJaguar/Bingus/Bingus.java
+++ src/main/java/net/ImmenseJaguar/Bingus/Bingus.java
@@ -1,5 +1,7 @@
 package net.ImmenseJaguar.Bingus;
 
+import net.ImmenseJaguar.Bingus.block.ModBlocks;
+import net.ImmenseJaguar.Bingus.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -56,6 +58,10 @@ public class Bingus
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -71,7 +77,13 @@ public class Bingus
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BINGUSTOOTH);
+        }
 
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.BINGUSTOOTH_BLOCK);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
